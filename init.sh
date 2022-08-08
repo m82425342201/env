f98687fd72d0bd2473d3ad934499072334585a92
m82425342201 <m82425342201@gmail.com>
2022-08-08 02:59:48 +0000

    ...

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
-cp /workspace/Env/gitconfig ~/.gitconfig
+cp /workspace/env/gitconfig ~/.gitconfig
 cd /workspace
 sudo apt update
 sudo apt install openssh-server rsync sshpass -y
 echo gitpod:Behd903gh|sudo chpasswd
 sudo /etc/init.d/ssh start
 #sh /workspace/pkgs2/install.sh
-sudo cp /workspace/Env/ngrok /usr/local/bin
+sudo cp /workspace/env/ngrok /usr/local/bin
 sudo chmod 755 /usr/local/bin/ngrok
 sudo locale-gen ko_KR.UTF-8
