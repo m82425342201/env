b4ed468eff6d006f43f1a430f169e20d93513933
m82425342201 <m82425342201@gmail.com>
2022-09-04 20:52:37 +0900

    Update install-ghcup.sh

--- install-ghcup.sh
+++ install-ghcup.sh
@@ -2,6 +2,12 @@ cd /tmp
 rm -rf $HOME/.cabal
 rm -rf $HOME/.ghcup
 rm -rf $HOME/.stack
+mkdir -p /workspace/ghcup/.cabal
+mkdir -p /workspace/ghcup/.ghcup
+mkdir -p /workspace/ghcup/.stack
+ln -s /workspace/ghcup/.cabal $HOME
+ln -s /workspace/ghcup/.ghcup $HOME
+ln -s /workspace/ghcup/.stack $HOME
 curl --proto 'https' -tlsv1.2 -sSf https://downloads.haskell.org/~ghcup/x86_64-linux-ghcup > ./ghcup
 chmod 755 ./ghcup
 sudo mv ./ghcup /usr/local/bin
@@ -12,9 +18,3 @@ ghcup install stack
 sudo rm -f /usr/local/bin/stack
 sudo ln -s /home/gitpod/.ghcup/bin/stack /usr/local/bin/stack
 #stack install ghcid
-mkdir -p /workspace/ghcup/.cabal
-mkdir -p /workspace/ghcup/.ghcup
-mkdir -p /workspace/ghcup/.stack
-ln -s /workspace/ghcup/.cabal $HOME
-ln -s /workspace/ghcup/.ghcup $HOME
-ln -s /workspace/ghcup/.stack $HOME
