27d4f2970e7a7a7ede54afd6deb751628c5a39e0
m82425342201 <m82425342201@gmail.com>
2022-09-15 18:53:10 +0900

    Delete git-clone-stock.sh

--- git-clone-stock.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /workspace/stocks
-cd /workspace/stocks
-git clone https://gitlab.com/jayeonsoo/ex-playwright-1
-git clone https://gitlab.com/m8228308850/alphasquare_downloader
-git clone https://gitlab.com/m8228308850/stock_naver
-git clone https://gitlab.com/m8228308850/stock_daum
-git clone https://gitlab.com/m8228308850/stock_krx
